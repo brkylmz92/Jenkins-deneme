3788624cc7768f48cec115271a919651f31bccd3
Burak <brkylmz92@gmail.com>
2026-02-25 13:03:24 +0300

    yeni versiyon

---
+++ src/test/java/tests/jenkins.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class jenkins {
+
+    @Test
+    void Test11() { assertTrue(true); }
+}
